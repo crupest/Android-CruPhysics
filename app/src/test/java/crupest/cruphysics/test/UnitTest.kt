7b80dae6be0b7d4ffca02cd69e959832e6f1110f
crupest <crupest@outlook.com>
2018-10-24 19:44:31 +0800

    Add robolectric.

--- app/src/test/java/crupest/cruphysics/test/UnitTest.kt
+++ app/src/test/java/crupest/cruphysics/test/UnitTest.kt
@@ -1,24 +1,27 @@
-package crupest.cruphysics
+package crupest.cruphysics.test
 
-import androidx.test.filters.SmallTest
-import androidx.test.runner.AndroidJUnit4
 import crupest.cruphysics.physics.fromData
 import crupest.cruphysics.physics.toData
 import crupest.cruphysics.serialization.data.CameraData
 import crupest.cruphysics.serialization.data.Vector2Data
-import org.hamcrest.Matchers.*
+import org.hamcrest.Matchers
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-@RunWith(AndroidJUnit4::class)
-@SmallTest
-class AndroidUnitTest {
+@RunWith(RobolectricTestRunner::class)
+class UnitTest {
+    @Test
+    fun test() {
+
+    }
+
     @Test
     fun cameraTest() {
         val cameraData = CameraData(Vector2Data(100.0, 100.0), 500.0)
         val convertBack = cameraData.fromData(100.0f, 100.0f).toData(100.0f, 100.0f)
 
-        Assert.assertThat(convertBack, equalTo(cameraData))
+        Assert.assertThat(convertBack, Matchers.equalTo(cameraData))
     }
 }
